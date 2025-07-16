c1dd95dcab71bafeea27fd243689833b2875e2ee
UmeshaJayakody <jayakodyjauc.22@uom.lk>
2025-07-16 18:50:53 +0530

    initialize frontend project with Vite, React, and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-gray-100 flex items-center justify-center h-screen font-sans text-2xl text-gray-800 tracking-wide">
+      Welcome!
+    </div>
+  )
+}
+
+export default App
