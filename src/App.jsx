557b31669865e22b3949aa7be5ce190dc2993e7b
UmeshaJayakody <jayakodyjauc.22@uom.lk>
2025-07-16 19:41:54 +0530

    Fix import path for Homepage component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
-import Homepage from './Pages/Homepage.jsx'
+import Homepage from './pages/Homepage.jsx'
 
 function App() {
   return (
